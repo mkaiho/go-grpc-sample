18ce895bc8692d97a49feda3d5e251e6616c53a7
Masahiko Kaiho <mkaiho.cgengo@gmail.com>
2021-09-23 05:12:57 +0000

    Add app server definition with terraform

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
 # go-grpc-sample
+
+## Buid
+
+```.sh
+make build-example
+```
+
+## Start server
+
+```.sh
+./build/example_server
+```
+
+## Execute client request
+
+```.sh
+# When you request to EC2 server, you need to modify constant "address" in ./cmd/example_client/main.go and rebuild
+./build/example_client
+```
+
+## Deploy EC2
+
+### Prerequisite
+
+- AWS Account has created already
+- The domain has already been acquired
+- Host Zone at Route53 has already been created and configured.
+
+### Set aws configuration
+
+```.sh
+# AWS Access Key ID [None]: <your access key id>
+# AWS Secret Access Key [None]: <your secret access key>
+# Default region name [None]: ap-northeast-1
+# Default output format [None]: json
+aws configure
+```
+
+### Generate ssh key to connect ec2 instance
+
+```.sh
+ssh-keygen -t rsa -m PEM -f ./.ssh/<your key name>
+```
+
+### Set environment variables for terraform parameters
+
+```.sh
+export TF_VAR_webapp_1a_key_name=<your key pair name to connect EC2 instance>
+export TF_VAR_webapp_1a_ssh_public_key=$(cat ./.ssh/<your public key name>
+export TF_VAR_zone_name=<host zone name in route53>
+```
+
+### Initialize terraform
+
+```.sh
+cd ./terraform
+terraform init
+```
+
+### Create or change app server and netowrk
+
+```.sh
+cd ./terraform
+terraform apply
+```
+
+### Start server on EC2
+
+```.sh
+# upload your app binary
+scp -i ./.ssh/<your private key name> -r ./build ec2-user@<host>:~/build
+# login host and run app server
+ssh -i ./.ssh/<your private key name> ec2-user@<host>
+./build/example_server
+```
+
+### Destroy app server and netowrk
+
+```.sh
+cd ./terraform
+terraform destroy
+```
---
+++ terraform/main.tf
@@ -0,0 +1,139 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+variable "zone_name" {
+  type = string
+}
+
+variable "webapp_1a_key_name" {
+  type = string
+}
+
+variable "webapp_1a_ssh_public_key" {
+  type = string
+}
+
+data "aws_ami" "ref_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0*"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_route53_zone" "webapp_app_dns" {
+  name = var.zone_name
+}
+
+resource "aws_vpc" "webapp_net" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    "Name" = "grpc-samples-net"
+  }
+}
+
+resource "aws_internet_gateway" "webapp_net_igw" {
+  vpc_id = aws_vpc.webapp_net.id
+  tags = {
+    "Name" = "grpc-samples-igw"
+  }
+}
+
+resource "aws_subnet" "webapp_net_public_1a" {
+  vpc_id                  = aws_vpc.webapp_net.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "webapp_route_public" {
+  vpc_id = aws_vpc.webapp_net.id
+  tags = {
+    "Name" = "grpc-samples-route-public"
+  }
+}
+
+resource "aws_route" "webapp_route_public" {
+  route_table_id         = aws_route_table.webapp_route_public.id
+  gateway_id             = aws_internet_gateway.webapp_net_igw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "webapp_route_public_1a" {
+  subnet_id      = aws_subnet.webapp_net_public_1a.id
+  route_table_id = aws_route_table.webapp_route_public.id
+}
+
+resource "aws_security_group" "webapp_app_sg" {
+  vpc_id = aws_vpc.webapp_net.id
+  tags = {
+    "Name" = "grpc-samples-webapp-sg"
+  }
+}
+
+resource "aws_security_group_rule" "webapp_app_sg_in_ssh" {
+  security_group_id = aws_security_group.webapp_app_sg.id
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "webapp_app_sg_in_app" {
+  security_group_id = aws_security_group.webapp_app_sg.id
+  type              = "ingress"
+  from_port         = 3000
+  to_port           = 3000
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "webapp_app_sg_out" {
+  security_group_id = aws_security_group.webapp_app_sg.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_key_pair" "webapp_app_1a_key_name" {
+  key_name   = var.webapp_1a_key_name
+  public_key = var.webapp_1a_ssh_public_key
+}
+
+resource "aws_instance" "webapp_app_1a" {
+  ami                    = data.aws_ami.ref_ami.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.webapp_net_public_1a.id
+  vpc_security_group_ids = [aws_security_group.webapp_app_sg.id]
+  key_name               = aws_key_pair.webapp_app_1a_key_name.id
+  tags = {
+    "Name" = "grpc-samples-webapp"
+  }
+}
+
+resource "aws_route53_record" "webapp_app_dns" {
+  zone_id = data.aws_route53_zone.webapp_app_dns.zone_id
+  name    = data.aws_route53_zone.webapp_app_dns.name
+  type    = "A"
+  ttl     = "300"
+  records = [aws_instance.webapp_app_1a.public_ip]
+  depends_on = [
+    aws_instance.webapp_app_1a
+  ]
+}
